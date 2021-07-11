e821f4ee6f16fabf49849f4cc04dc37e95946255
monoharada <harada@monosus.co.jp>
2021-07-11 07:54:58 +0000

    add prisma next-auth changre lint rule

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import Adapters from "next-auth/adapters";
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next-auth/internals/utils";
+
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+const options = {
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+};
+
+export default (req: NextApiRequest, res: NextApiResponse<any>) => NextAuth(req, res, options);
